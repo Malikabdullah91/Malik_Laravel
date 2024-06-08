4f50965997754f7842b2e3b352b2084ccb70afdf
Malikabdullah91 <alekgtfc8@gmail.com>
2024-06-08 20:21:36 +0700

    Initial commit

---
+++ resources/views/categories/categories-hapus.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('title')
+Hapus Category | OTW Admin
+@endsection
+
+@section('content')
+<h3>Hapus Categories</h3>
+<div class="form-login">
+  <h4>Ingin Menghapus Data ?</h4>
+  <button type="submit" class="btn btn-simpan" name="hapus" style="width: 40%; margin: 20px auto;">
+  <a href="{{ url('/category/destroy/' . $category->id_categories ) }}"></a>
+  </button>
+  <button type="submit" class="btn btn-simpan" name="tidak" style="width: 40%; margin: 20px auto;">
+    <a href="/category">
+      No
+    </a>
+  </button>
+</div>
+@endsection
